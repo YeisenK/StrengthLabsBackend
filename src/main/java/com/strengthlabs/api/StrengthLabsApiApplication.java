c73d758b2f2ad393ca07fbe26def12e618cb2636
yeisenk <yeisen.kenneth@gmail.com>
2026-03-10 20:06:33 +0000

    feat: initial Clean Architecture structure

---
+++ src/main/java/com/strengthlabs/api/StrengthLabsApiApplication.java
@@ -0,0 +1,13 @@
+package com.strengthlabs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrengthLabsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrengthLabsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strengthlabs/api/StrengthLabsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strengthlabs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrengthLabsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
